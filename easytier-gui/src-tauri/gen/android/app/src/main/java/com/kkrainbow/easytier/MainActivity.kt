ed2982221b3765a3b0b1995730f2f3e9aad5936f
Administrator <hxl123789@163.com>
2025-04-18 09:54:23 +0800

    删除proxy

--- easytier-gui/src-tauri/gen/android/app/src/main/java/com/kkrainbow/easytier/MainActivity.kt
+++ easytier-gui/src-tauri/gen/android/app/src/main/java/com/kkrainbow/easytier/MainActivity.kt
@@ -1,19 +1,3 @@
 package com.kkrainbow.easytier
-import android.content.Intent
-import android.os.Build
-import android.os.Bundle
-import androidx.annotation.RequiresApi
-class MainActivity : TauriActivity(){
-  
-    override fun onCreate(savedInstanceState: Bundle?) {
-      super.onCreate(savedInstanceState)
 
-
-      // 启动服务
-      Intent(this, ProxyService::class.java).apply {
-        
-          startForegroundService(this)
-        
-      }
-    }
-}
+class MainActivity : TauriActivity()
